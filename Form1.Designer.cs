2df6bc20af3e8c2c322f3189890ab190aee13011
Visuvi <58734926+Visuvi@users.noreply.github.com>
2021-12-06 19:08:00 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace TVDBAdder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtActor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReleased = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLanguage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtAir = new System.Windows.Forms.TextBox();
+            this.txtDayAiring = new System.Windows.Forms.Label();
+            this.txtRetDate = new System.Windows.Forms.TextBox();
+            this.txtReturning = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(78, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTitle.Location = new System.Drawing.Point(170, 118);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 27);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtActor
+            // 
+            this.txtActor.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtActor.Location = new System.Drawing.Point(170, 164);
+            this.txtActor.Name = "txtActor";
+            this.txtActor.Size = new System.Drawing.Size(200, 27);
+            this.txtActor.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(78, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Actor:";
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtGenre.Location = new System.Drawing.Point(170, 209);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(200, 27);
+            this.txtGenre.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(78, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Genre:";
+            // 
+            // txtReleased
+            // 
+            this.txtReleased.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtReleased.Location = new System.Drawing.Point(170, 258);
+            this.txtReleased.Name = "txtReleased";
+            this.txtReleased.Size = new System.Drawing.Size(200, 27);
+            this.txtReleased.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(60, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 30);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Released:";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDuration.Location = new System.Drawing.Point(170, 306);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(200, 27);
+            this.txtDuration.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(49, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 30);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Duration:";
+            // 
+            // txtLanguage
+            // 
+            this.txtLanguage.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLanguage.Location = new System.Drawing.Point(170, 355);
+            this.txtLanguage.Name = "txtLanguage";
+            this.txtLanguage.Size = new System.Drawing.Size(200, 27);
+            this.txtLanguage.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(49, 352);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 30);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Language:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSubmit.Location = new System.Drawing.Point(179, 489);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(163, 53);
+            this.btnSubmit.TabIndex = 17;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtAir
+            // 
+            this.txtAir.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAir.Location = new System.Drawing.Point(170, 404);
+            this.txtAir.Name = "txtAir";
+            this.txtAir.Size = new System.Drawing.Size(200, 27);
+            this.txtAir.TabIndex = 13;
+            // 
+            // txtDayAiring
+            // 
+            this.txtDayAiring.AutoSize = true;
+            this.txtDayAiring.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtDayAiring.Location = new System.Drawing.Point(72, 401);
+            this.txtDayAiring.Name = "txtDayAiring";
+            this.txtDayAiring.Size = new System.Drawing.Size(80, 30);
+            this.txtDayAiring.TabIndex = 16;
+            this.txtDayAiring.Text = "Airing:";
+            // 
+            // txtRetDate
+            // 
+            this.txtRetDate.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtRetDate.Location = new System.Drawing.Point(170, 454);
+            this.txtRetDate.Name = "txtRetDate";
+            this.txtRetDate.Size = new System.Drawing.Size(200, 27);
+            this.txtRetDate.TabIndex = 15;
+            // 
+            // txtReturning
+            // 
+            this.txtReturning.AutoSize = true;
+            this.txtReturning.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtReturning.Location = new System.Drawing.Point(16, 451);
+            this.txtReturning.Name = "txtReturning";
+            this.txtReturning.Size = new System.Drawing.Size(136, 30);
+            this.txtReturning.TabIndex = 18;
+            this.txtReturning.Text = "Return Date:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(79, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 131);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(116, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(293, 99);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 554);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtRetDate);
+            this.Controls.Add(this.txtReturning);
+            this.Controls.Add(this.txtAir);
+            this.Controls.Add(this.txtDayAiring);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtLanguage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtReleased);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtActor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tv Shows";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtActor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReleased;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDuration;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLanguage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtAir;
+        private System.Windows.Forms.Label txtDayAiring;
+        private System.Windows.Forms.TextBox txtRetDate;
+        private System.Windows.Forms.Label txtReturning;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TVDBAdder
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=TvRegistration;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("TvAdd",sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@Title",txtTitle.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Actor",txtActor.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Genre",txtGenre.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Released",txtReleased.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Duration",txtDuration.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Language",txtLanguage.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@DayAiring",txtAir.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@RetDate", txtRetDate.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Registration is Successful");
+                Clear();
+            }
+        }
+
+        void Clear()
+        {
+            txtTitle.Text = txtActor.Text = txtGenre.Text = txtReleased.Text = txtDuration.Text = txtLanguage.Text = txtAir.Text = txtRetDate.Text = "";
+        }
+    }
+}
